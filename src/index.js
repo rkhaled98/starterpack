d51af939fa80da255be74570aafc75210c20b7bd
Rafi Khaled <rkhaled@vassar.edu>
2019-04-13 01:17:23 -0400

    hot reloading

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // change require to es6 import style
 import $ from 'jquery';
+import './style.scss';
 
 const mainDiv = $('#main');
 let num = 0;
 
-setInterval(() =>{
-    count = count + 1;
-    mainDiv.text(`You've been on this page for ${num} seconds`)
-}, 1000)
+setInterval(() => {
+  num += 1;
+  mainDiv.text(`You've been on this page for ${num} seconds`);
+}, 1000);
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,53 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const env = process.env.NODE_ENV || 'development';
 // set to 'production' or 'development' in your env
 
-module.exports = {
-    mode: env,
-    entry: [ 'babel-polyfill', './src' ], // this is where our app lives
-    devtool: 'source-map', // this enables debugging with source in chrome devtools
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [ //here's the part you want
-                    { loader: 'babel-loader' }
-                ]
-            }
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: './index.html',
-        }),
-    ],
-};
+const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { loader: 'style-loader' };
 
+module.exports = {
+  mode: env,
+  entry: ['babel-polyfill', './src'], // this is where our app lives
+  devtool: 'source-map', // this enables debugging with source in chrome devtools
+  devServer: {
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          { loader: 'eslint-loader' },
+        ],
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          finalCSSLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+};
