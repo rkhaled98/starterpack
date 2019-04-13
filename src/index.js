0e68ac133cd8a6e00bb14e4a28d4fefebb34eee5
Rafi Khaled <rkhaled@vassar.edu>
2019-04-13 17:12:45 -0400

    updated starterpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 // change require to es6 import style
-import $ from 'jquery';
+// import $ from 'jquery';
 import './style.scss';
 
-const mainDiv = $('#main');
-let num = 0;
+// const mainDiv = $('#main');
+// let num = 0;
 
-setInterval(() => {
-  num += 1;
-  mainDiv.text(`You've been on this page for ${num} seconds`);
-}, 1000);
+// setInterval(() => {
+//   num += 1;
+//   mainDiv.text(`You've been on this page for ${num} seconds`);
+// }, 1000);
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+ReactDOM.render(<App />, document.getElementById('main'));
