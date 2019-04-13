ef30c9e8394876219a2780aec12bd1e8f4ce528a
Rafi Khaled <rkhaled@vassar.edu>
2019-04-13 01:05:44 -0400

    counter working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const $ = require('jquery');
+// change require to es6 import style
+import $ from 'jquery';
 
-$('#main').html('Here we go!');
+const mainDiv = $('#main');
+let num = 0;
+
+setInterval(() =>{
+    count = count + 1;
+    mainDiv.text(`You've been on this page for ${num} seconds`)
+}, 1000)
