1a3ecd79665a7c0196dbd6acb50dab94f9464fcd
Rafi Khaled <rkhaled@vassar.edu>
2019-04-13 01:29:57 -0400

    done

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const env = process.env.NODE_ENV || 'development';
+const autoprefixer = require('autoprefixer');
 // set to 'production' or 'development' in your env
 
 const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { loader: 'style-loader' };
@@ -34,9 +35,28 @@ module.exports = {
             },
           },
           {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()],
+              sourceMap: true,
+            },
+          },
+          {
             loader: 'sass-loader',
             options: {
               sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[name].[ext]',
             },
           },
         ],
