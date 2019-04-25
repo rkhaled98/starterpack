a9a1c571751f0c3d16abef5b526ef4a2a618120a
Rafi Khaled <rkhaled@vassar.edu>
2019-04-25 13:56:35 -0400

    update file structure
    export App to root index.js

---
+++ src/components/app.js
@@ -0,0 +1,83 @@
+// change require to es6 import style
+// import $ from 'jquery';
+import '../style.scss';
+
+// const mainDiv = $('#main');
+// let num = 0;
+
+// setInterval(() => {
+//   num += 1;
+//   mainDiv.text(`You've been on this page for ${num} seconds`);
+// }, 1000);
+
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import { Switch } from 'react-router';
+
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Welcome} />
+            <Route path="/about" component={About} />
+            <Route exact path="/test/:id" component={Test} />
+            <Route component={FallBack} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+// export default const App = (props) => {
+//   return (
+//     <Router>
+//       <div>
+//         <Nav />
+//         <Switch>
+//           <Route exact path="/" component={Welcome} />
+//           <Route path="/about" component={About} />
+//           <Route exact path="/test/:id" component={Test} />
+//           <Route component={FallBack} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// };
+
+ReactDOM.render(<App />, document.getElementById('main'));
